b18fe267962addcb5a3da6c63747e46cfb68a1e5
Origami Official <64251776+origamiofficial@users.noreply.github.com>
2022-12-23 02:01:14 +0600

    Major Update V2
    * Added new variables NEW_NOTICE_MESSAGE_FORMAT and EDITED_NOTICE_MESSAGE_FORMAT at the top of the script. These variables define the format of messages to be sent by the script for new and edited notices, respectively.
    * Script now uses the Links as unique identifier to check if notice has been seen before as the links never changes.
    * Now the script delete all previous notices from the database because they moved to second page of the notice website.
    * The script now logs the number of posts found on the AIUB Notice page.
    * The script now includes error handling and debugging output to help troubleshoot any issues that may occur.

--- main.py
+++ main.py
@@ -17,12 +17,28 @@ DAY_XPATH = ".//time/span[@class='day']/text()"
 MONTH_XPATH = ".//time/span[@class='month']/text()"
 YEAR_XPATH = ".//time/span[@class='year']/text()"
 
+# Message format for new notices
+NEW_NOTICE_MESSAGE_FORMAT = (
+    "{title}\n\n"
+    "Date: {day} {month} {year}\n\n"
+    "{description}\n\n"
+    "https://www.aiub.edu{link}"
+)
+
+# Message format for edited notices
+EDITED_NOTICE_MESSAGE_FORMAT = (
+    "[Edited] {title}\n\n"
+    "Date: {day} {month} {year}\n\n"
+    "{description}\n\n"
+    "https://www.aiub.edu{link}"
+)
+
 # SQLite database information
 DB_NAME = "aiub_notices.db"
 DB_TABLE_NAME = "notices"
 
 # Script version
-SCRIPT_VERSION = "1.8"
+SCRIPT_VERSION = "2.0"
 SCRIPT_URL = "https://raw.githubusercontent.com/origamiofficial/aiub-notice-checker/main/main.py"
 
 # Check for script updates
@@ -74,96 +90,126 @@ except Exception as e:
 if os.path.exists(DB_NAME):
     print(f"Existing SQLite database file found.")
 else:
-    print(f"Existing SQLite database file not found, created one")
-
-# Connect to SQLite database
-print("Connecting to SQLite database...")
-try:
+    print(f"Existing SQLite database file not found. Creating new file...")
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
-    print("Connection to database successful.")
-except Exception as e:
-    print(f"Error connecting to database: {e}. Exiting script.")
-    exit()
-
-# Check if notices table exists in database, and create it if it doesn't
-try:
     c.execute(
-        "CREATE TABLE IF NOT EXISTS {} (title text, description text, link text)".format(
-            DB_TABLE_NAME
-        )
+        f"CREATE TABLE {DB_TABLE_NAME} ("
+        "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+        "title TEXT NOT NULL,"
+        "description TEXT NOT NULL,"
+        "link TEXT NOT NULL,"
+        "day INTEGER NOT NULL,"
+        "month INTEGER NOT NULL,"
+        "year INTEGER NOT NULL"
+        ")"
     )
-except Exception as e:
-    print(f"Error creating notices table in database: {e}. Exiting script.")
-    exit()
+    conn.commit()
+    conn.close()
 
-# Check if table was created or already exists
-try:
-    c.execute("SELECT * FROM sqlite_master WHERE type='table' AND name=?", (DB_TABLE_NAME,))
-    if c.fetchone():
-        print("Notices table already exists in database.")
-    else:
-        print("Notices table created in database.")
-except Exception as e:
-    print(f"Error checking if notices table exists: {e}. Exiting script.")
-    exit()
+# Connect to database
+conn = sqlite3.connect(DB_NAME)
+c = conn.cursor()
 
-# Define the send_telegram_message function
-def send_telegram_message(title, description, link, day, month, year):
-    # Use the telegram information provided in the script to construct the URL for the API
-    telegram_api_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_API_KEY}/sendMessage"
+# Send message to Telegram chat
+def send_telegram_message(message):
+    # Send message to Telegram chat
+    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_API_KEY}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+		"disable_web_page_preview": "true"
+    }
+    try:
+        response = requests.post(url, json=payload)
+        print(f"Sending notice: {title}")
+        # Check if the request was successful, and print the response from the server
+        if response.status_code == 200:
+            print(f"Successfully sent message to Telegram.")
+        else:
+            print(
+                f"Error sending message to Telegram: {response.text}. Exiting script."
+            )
+            exit()
+    except Exception as e:
+        print(f"Error sending message to Telegram: {e}")
 
-    # Use the URL and XPath information provided in the script to extract the title, description, and link
-    # for each post on the AIUB Notice page
-    message = f"{title}\n\nDate: {day} {month} {year}\n\n{description}\n\nhttps://www.aiub.edu{link}"
-
-    # Use the requests module to send a POST request to the telegram API URL with the necessary
-    # parameters to send a message to the specified chat
-    response = requests.post(
-        telegram_api_url,
-        data={"chat_id": TELEGRAM_CHAT_ID, "text": message, "disable_web_page_preview": True},
-    )
-
-    # Check if the request was successful, and print the response from the server
-    if response.status_code == 200:
-        print(f"Successfully sent message to Telegram.")
-    else:
-        print(
-            f"Error sending message to Telegram: {response.text}. Exiting script."
-        )
-        exit()
-
-# Iterate through each post on the AIUB Notice page and check if it exists in the database
+# Iterate through posts and check for new or edited notices
 for post in posts:
-    # Use the XPath expressions provided in the script to extract the title, description, link, day, month, and year
-    # for each post
-    title = post.xpath(TITLE_XPATH)[0]
-    description = post.xpath(DESCRIPTION_XPATH)[0]
-    link = post.xpath(LINK_XPATH)[0]
-    day = post.xpath(DAY_XPATH)[0]
-    month = post.xpath(MONTH_XPATH)[0]
-    year = post.xpath(YEAR_XPATH)[0]
-
-    # Check if post is already in database
-    c.execute(
-        "SELECT * FROM {} WHERE title=? AND description=? AND link=?".format(
-            DB_TABLE_NAME
-        ),
-        (title, description, link),
-    )
-    # Check if the query returned any rows
-    if c.fetchone() is None:
-        # Send post to Telegram
-        send_telegram_message(title, description, link, day, month, year)
-        print(f"Sent post to Telegram: {title}")
-        # Insert post into database
+    # Retrieve data for post
+    title = "".join(post.xpath(TITLE_XPATH)).strip()
+    link = "".join(post.xpath(LINK_XPATH)).strip()
+    description = "".join(post.xpath(DESCRIPTION_XPATH)).strip()
+    day = "".join(post.xpath(DAY_XPATH)).strip()
+    month = "".join(post.xpath(MONTH_XPATH)).strip()
+    year = "".join(post.xpath(YEAR_XPATH)).strip()
+    # Check if notice has been seen before
+    c.execute(f"SELECT * FROM {DB_TABLE_NAME} WHERE link=?", (link,))
+    result = c.fetchone()
+    if result is None:
+        # Notice is new, add to database and send message
         c.execute(
-            "INSERT INTO {} (title, description, link) VALUES (?, ?, ?)".format(
-                DB_TABLE_NAME
-            ),
-            (title, description, link),
+            f"INSERT INTO {DB_TABLE_NAME} "
+            "(title, description, link, day, month, year) VALUES (?,?,?,?,?,?)",
+            (title, description, link, day, month, year)
         )
+        conn.commit()
+        message = NEW_NOTICE_MESSAGE_FORMAT.format(
+            title=title,
+            day=day,
+            month=month,
+            year=year,
+            description=description,
+            link=link
+        )
+        send_telegram_message(message)
+    else:
+        # Notice has been seen before, check if it has been edited
+        old_title = result[1]
+        old_description = result[2]
+        if old_title != title or old_description != description:
+            # Notice has been edited, update database and send message
+            c.execute(
+                f"UPDATE {DB_TABLE_NAME} "
+                "SET title=?, description=? "
+                "WHERE link=?",
+                (title, description, link)
+            )
+            conn.commit()
+            message = EDITED_NOTICE_MESSAGE_FORMAT.format(
+                title=title,
+                day=day,
+                month=month,
+                year=year,
+                description=description,
+                link=link
+            )
+            send_telegram_message(message)
 
-# Save changes to database and close connection
+# Check for moved notices
+c.execute(f"SELECT link, title FROM {DB_TABLE_NAME}")
+results = c.fetchall()
+for result in results:
+    link = result[0]
+    title = result[1]
+    # Check if link is still present on AIUB Notice page
+    page = requests.get(WEBSITE_URL)
+    tree = html.fromstring(page.content)
+    posts = tree.xpath(POST_XPATH)
+    found = False
+    for post in posts:
+        post_link = post.xpath(LINK_XPATH)[0]
+        if post_link == link:
+            found = True
+            break
+    if not found:
+        # Link not found on AIUB Notice page, delete from database
+        c.execute(f"DELETE FROM {DB_TABLE_NAME} WHERE link=?", (link,))
+        conn.commit()
+        print(f"Notice '{title}' has been deleted.")
+
+# Close database connection
 conn.commit()
-conn.close
+conn.close()
+
+print("Script completed.")
